f6c9273d2007802d9fdcbcc171e5bef4d1aef3df
Kennethrc2401 <kennethramirezcastro@gmail.com>
2018-03-06 18:23:30 -0500

    URI

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -3,7 +3,7 @@ const spotifySearchAPI = 'https://api.spotify.com/v1/search';
 const spotifyUserProfileAPI = 'https://api.spotify.com/v1/me';
 const spotifyPlaylistAPI = 'https://api.spotify.com/v1/users/${userId}/playlists';
 const spotifyPlaylistTracksAPI = 'https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks';
-const spotifyRedirectUrl = "http://localhost:3000/callback";
+const spotifyRedirectUrl = "http://localhost:3001/callback";
 
 let accessToken;
 let expiresIn;
